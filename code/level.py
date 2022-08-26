a92c6384f76794a1ad6fe86828d580479678fdba
Snaptraks <snaptraks@gmail.com>
2022-08-25 21:53:58 -0400

    Importing all elements and faking more 3D

--- code/level.py
+++ code/level.py
@@ -1,9 +1,11 @@
 import pygame
+from pytmx.util_pygame import load_pygame
 
 from player import Player
 from overlay import Overlay
-from sprites import Generic
-from settings import ROOT, SCREEN_HEIGHT, SCREEN_WIDTH, LAYERS
+from support import import_folder
+from sprites import Generic, Water, WildFlower, Tree
+from settings import ROOT, SCREEN_HEIGHT, SCREEN_WIDTH, LAYERS, TILE_SIZE
 
 
 class Level:
@@ -18,6 +20,47 @@ class Level:
         self.overlay = Overlay(self.player)
 
     def setup(self) -> None:
+        tmx_data = load_pygame(str(ROOT / "data/map.tmx"))
+
+        # house
+        for layer in ("HouseFloor", "HouseFurnitureBottom"):
+            for x, y, surface in tmx_data.get_layer_by_name(layer).tiles():
+                Generic(
+                    (x * TILE_SIZE, y * TILE_SIZE),
+                    surface,
+                    self.all_sprites,
+                    LAYERS["house_bottom"],
+                )
+
+        for layer in ("HouseWalls", "HouseFurnitureTop"):
+            for x, y, surface in tmx_data.get_layer_by_name(layer).tiles():
+                Generic(
+                    (x * TILE_SIZE, y * TILE_SIZE),
+                    surface,
+                    self.all_sprites,
+                )
+
+        # fence
+        for x, y, surface in tmx_data.get_layer_by_name("Fence").tiles():
+            Generic(
+                (x * TILE_SIZE, y * TILE_SIZE),
+                surface,
+                self.all_sprites,
+            )
+
+        # water
+        water_frames = import_folder(ROOT / "graphics/water")
+        for x, y, surface in tmx_data.get_layer_by_name("Water").tiles():
+            Water((x * TILE_SIZE, y * TILE_SIZE), water_frames, self.all_sprites)
+
+        # trees
+        for obj in tmx_data.get_layer_by_name("Trees"):
+            Tree((obj.x, obj.y), obj.image, self.all_sprites, name=obj.name)
+
+        # wildflowers
+        for obj in tmx_data.get_layer_by_name("Decoration"):
+            WildFlower((obj.x, obj.y), obj.image, self.all_sprites)
+
         self.player = Player((SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), self.all_sprites)
         Generic(
             position=(0, 0),
@@ -46,7 +89,7 @@ class CameraGroup(pygame.sprite.Group):
         self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
         self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
-        sprites = sorted(self.sprites(), key=lambda s: s.z)  # type: ignore
+        sprites = sorted(self.sprites(), key=lambda s: (s.z, s.rect.centery))  # type: ignore # noqa
         for sprite in sprites:
             offset_rect = sprite.rect.copy()  # type:ignore
             offset_rect.center -= self.offset  # type: ignore
--- code/sprites.py
+++ code/sprites.py
@@ -14,3 +14,49 @@ class Generic(pygame.sprite.Sprite):
         self.image = surface
         self.rect = self.image.get_rect(topleft=position)
         self.z = z
+
+
+class Water(Generic):
+    def __init__(
+        self,
+        position: tuple[int, ...],
+        frames: list[pygame.surface.Surface],
+        groups: pygame.sprite.Group,
+    ) -> None:
+        # animation setup
+        self.frames = frames
+        self.frame_index: float = 0
+
+        # sprite setup
+        super().__init__(
+            position, self.frames[self.frame_index], groups, LAYERS["water"]
+        )
+
+    def animate(self, dt: float):
+        self.frame_index += 5 * dt
+        self.frame_index %= len(self.frames)
+        self.image = self.frames[int(self.frame_index)]
+
+    def update(self, dt: float):
+        self.animate(dt)
+
+
+class WildFlower(Generic):
+    def __init__(
+        self,
+        position: tuple[int, ...],
+        surface: pygame.surface.Surface,
+        groups: pygame.sprite.Group,
+    ) -> None:
+        super().__init__(position, surface, groups)
+
+
+class Tree(Generic):
+    def __init__(
+        self,
+        position: tuple[int, ...],
+        surface: pygame.surface.Surface,
+        groups: pygame.sprite.Group,
+        name: str,
+    ) -> None:
+        super().__init__(position, surface, groups)
